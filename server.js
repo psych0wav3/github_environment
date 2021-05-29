e90fb094e76d57850ef994e974ad34f998864862
psych0wav3 <geov.ane@hotmail.com>
2021-05-29 20:25:32 -0300

    teste

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.status(200).json({status: "Funcionando"})
+})
+
+app.listen(3333)
+
