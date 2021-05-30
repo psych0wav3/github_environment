dc32437256f75b3ed44e103d182da151c22a3582
psych0wav3 <geov.ane@hotmail.com>
2021-05-29 21:54:08 -0300

    jest false

--- server.spec.js
+++ server.spec.js
@@ -1,6 +1,6 @@
 describe("teste falso para github Action", ()=>{
     it("vai retornar falso", ()=>{
-        const valor = true
+        const valor = false
         const result = true
         expect(valor).toBe(result)
     })
